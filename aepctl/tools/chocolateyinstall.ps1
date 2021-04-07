e8b09d43c711a0b2d493ef89e808baf05cdb97fb
fuxs <michael@bungenstock.de>
2021-04-07 14:55:12 +0200

    New version v0.2.0

--- aepctl/tools/chocolateyinstall.ps1
+++ aepctl/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://www.bungenstock.de/aepctl/releases/v0.1.1/aepctl-windows-386.tgz'
-  url64bit      = 'https://www.bungenstock.de/aepctl/releases/v0.1.1/aepctl-windows-amd64.tgz'
-  checksum      = 'b873f3f810566c9034b1c2fcb7118790eb314988546e52521df8112ea6bf12c6'
+  url           = 'https://www.bungenstock.de/aepctl/releases/v0.2.0/aepctl-windows-386.tgz'
+  url64bit      = 'https://www.bungenstock.de/aepctl/releases/v0.2.0/aepctl-windows-amd64.tgz'
+  checksum      = '0c1c102cf661fd2b928e14de1c2e334bc6360014dc4004deb94db9e99e8b193f'
   checksumType  = 'sha256'
-  checksum64    = '9fd44e1d0197b515ef53692f778b8d3ecc27195e0f407b7fb87a4433790d5f21'
+  checksum64    = '8f5330fba92dbbe6009b0ac42c11012e1ffd3ecc56a3d5fd140683415d6fb381'
   checksumType64= 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
